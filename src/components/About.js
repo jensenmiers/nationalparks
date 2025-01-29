ced34fcf81955fb4e61a254d90f2329dbad2fa7f
jensenmiers <jensenmiers@gmail.com>
2025-01-28 20:55:27 -0800

    change jensen url to itsjensen portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 function About(){
     return (
         <div style={{textAlign: 'center'}}>
-            <p>A guide to U.S. national parks built by <a href='https://github.com/jensenmiers'>Jensen Miers</a> and <a href='https://github.com/mattIshida'>Matt Ishida</a>.</p>
+            <p>A guide to U.S. national parks built by <a href='https://itsjensen.com'>Jensen Miers</a> and <a href='https://github.com/mattIshida'>Matt Ishida</a>.</p>
         </div>
     )
 }
