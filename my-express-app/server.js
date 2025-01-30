2a8687eb2bc44138c6be5131f2aa0da3f038dffa
jensenmiers <jensenmiers@gmail.com>
2025-01-29 20:40:56 -0800

    init express server

---
+++ my-express-app/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
