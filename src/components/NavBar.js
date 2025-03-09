ce5e4377177efd643c42ad8d0464ade3f8a1d5a9
jensenmiers <jensenmiers@gmail.com>
2025-03-09 12:04:25 -0700

    NavBar header change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 function NavBar({ userId }){
     return (
         <nav className='nav'>
-            <NavLink exact to='/'>Home</NavLink>
+            <NavLink exact to='/'>Welcome</NavLink>
             <NavLink to='/parks'>Browse</NavLink>
             <NavLink to={`/user/${userId}`}>MyParks</NavLink>
             <NavLink to='/about'>About</NavLink>
